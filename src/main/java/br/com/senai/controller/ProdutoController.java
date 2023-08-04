5ee9ea0c7b28ffe1c08d3370fdd3eaa022631caa
Augusto <augustobpinho@gmail.com>
2023-08-03 21:50:15 -0300

    comitando API

--- src/main/java/br/com/senai/controller/ProdutoController.java
+++ src/main/java/br/com/senai/controller/ProdutoController.java
@@ -3,9 +3,13 @@ package br.com.senai.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,9 +37,26 @@ public class ProdutoController {
 		return produtorepository.findAll();
 		
 	}
+	
 	@GetMapping("/{id}")
 	public ResponseEntity<Produto> findById(@PathVariable Long id) {
 		return produtorepository.findById(id).map(gravado -> ResponseEntity.ok().body(gravado)).orElse(ResponseEntity.notFound().build());
 	}
 	
+	@PostMapping
+	public ResponseEntity<Produto> cadastrarProduto(@RequestBody Produto produto){
+		return ResponseEntity.status(HttpStatus.CREATED).body(produtorepository.save(produto));
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Produto> atualizar(@PathVariable Long id, @RequestBody Produto produto){
+		return produtorepository.findById(id).map(gravado -> {
+			gravado.setNome(produto.getNome());
+			gravado.setCategoria(produto.getCategoria());
+			gravado.setQuantidade(produto.getQuantidade());
+			gravado.setPreco(produto.getPreco());
+			Produto atualizado = produtorepository.save(gravado);
+			return ResponseEntity.ok().body(atualizado);
+		}).orElse(ResponseEntity.notFound().build());
+	}
 }
